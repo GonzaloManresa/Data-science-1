81d76687f286e8666f86b5b0d6b18a2c5703532b
Gonzalo Manresa <manresagonzalo05@gmail.com>
2024-11-09 11:37:24 -0300

    Agregacion de codigo

---
+++ pycache/Menu.py
@@ -0,0 +1,16 @@
+import pycache.Plato_Menu as Plato_Menu
+
+class Menu:
+    def __init__(self):
+        self.platos = []
+        
+    def agregar_plato(self, nombre, descripcion, precio):
+        self.pedidos.append(Plato_Menu(nombre, descripcion, precio))
+    
+    def remover_plato(self, nombre):
+        self.pedidos = [plato for plato in self.platos if plato.nombre != nombre]
+        
+    def mostrar_menu(self):
+        for plato in self.platos:
+            print(f"{plato.nombre}: {plato.descripcion} - ${plato.precio}")
+    
